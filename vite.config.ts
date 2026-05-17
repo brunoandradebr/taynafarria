f725bdb6431137258defd1327c6e0fb31df676d5
Bruno Andrade <bruno.faria.andrade@gmail.com>
2026-05-17 20:45:57 -0300

    feat: add vite single file plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), viteSingleFile()],
 })
